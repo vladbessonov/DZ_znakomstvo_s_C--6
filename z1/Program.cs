556cfccedaa09c1deda26252120295a2f30b07ea
Vlad <vladvict2009@yandex.ru>
2023-02-06 20:28:39 +0400

    сохранение перед запушиванием

---
+++ z1/Program.cs
@@ -0,0 +1,67 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// Создаем массив заданного размера
+
+string message = $"Введите число:   ";
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreatArray(string message)
+{
+    int size = InputInt(message);
+    int[] array = new int[size];
+    return array;
+}
+// Функция заполнения массива
+int[] FillArray(int[] array, string message)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = InputInt(message);
+    }
+    return array;
+}
+// функция печати массива:
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}; "); // item - элемент массива
+    }
+    System.Console.WriteLine();
+}
+
+// Определение количества положительных чисел:
+int PozitivNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = CreatArray("Введите количество сравниваемых чисел");
+
+FillArray(array, message);
+Console.WriteLine($"Введённые числа:");
+PrintArray(array);
+int count = PozitivNum(array);
+Console.WriteLine($"Количество положительных чисел составляет - {count}");
+
+
+
+
+
---
+++ z2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Ввод данных:
+Console.Write("Введите коэффициент уравнения k1 =   ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент уравнения b1 =   ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент уравнения k2 =   ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент уравнения b2 =   ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double CoordX = 0;
+double CoordY = 0;
+
+if (k1 != k2) // Проверка на пересекаемость
+{
+    CoordX = ((b2 - b1) / (k1 - k2));
+    CoordY = k1 * CoordX + b1;
+    Console.WriteLine($"Прямые  пересекаются в точке с координатами х = {CoordX:F2}, y = {CoordY:F2}.");
+}
+else
+{
+    Console.WriteLine("Прямые параллельны.");
+}
+
+
+
+
+
+
+
+
+
+
